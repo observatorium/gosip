d1af3027e9bf0bdf2570a0f9bfadd77a00c36a70
Matthias Loibl <mail@matthiasloibl.com>
2019-02-20 21:29:37 +0100

    Move migrations into database/migrations/

--- database/migrations/1550595093_create_tenants_table.up.sql
+++ database/migrations/1550595093_create_tenants_table.up.sql
@@ -2,7 +2,6 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE tenants (
   id         UUID PRIMARY KEY      DEFAULT gen_random_uuid(),
-  name       VARCHAR(100) NOT NULL,
-  prometheus VARCHAR(256) NOT NULL,
-  jaeger     VARCHAR(256) NOT NULL
+  prometheus TEXT NOT NULL,
+  jaeger     TEXT NOT NULL
 );
