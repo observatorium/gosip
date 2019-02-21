76b77f51281069e59905f5ce50e15ad9501e5df7
Matthias Loibl <mail@matthiasloibl.com>
2019-02-21 17:56:04 +0100

    Add unique index on user's username and token

--- database/migrations/1550595093_create_tenants_table.up.sql
+++ database/migrations/1550595093_create_tenants_table.up.sql
@@ -1,7 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE TABLE tenants (
-  id         UUID PRIMARY KEY      DEFAULT gen_random_uuid(),
+CREATE TABLE tenants
+(
+  id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   prometheus TEXT NOT NULL,
   jaeger     TEXT NOT NULL
 );
--- database/migrations/1550598541_create_tenants_users_table.up.sql
+++ database/migrations/1550598541_create_tenants_users_table.up.sql
@@ -1,5 +1,6 @@
-CREATE TABLE tenants_users (
-  tenant_id   UUID REFERENCES tenants ON UPDATE CASCADE ON DELETE CASCADE,
-  user_id     UUID REFERENCES users   ON UPDATE CASCADE ON DELETE CASCADE,
+CREATE TABLE tenants_users
+(
+  tenant_id UUID REFERENCES tenants ON UPDATE CASCADE ON DELETE CASCADE,
+  user_id   UUID REFERENCES users ON UPDATE CASCADE ON DELETE CASCADE,
   CONSTRAINT tenants_users_pkey PRIMARY KEY (tenant_id, user_id)
 );
