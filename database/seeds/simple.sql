93421d5a92a66399dc77ffc1ffd73b11b639bdc1
Matthias Loibl <mail@matthiasloibl.com>
2019-02-21 17:54:57 +0100

    Rename Name() to Username() for user interface

--- database/seeds/simple.sql
+++ database/seeds/simple.sql
@@ -29,7 +29,7 @@ COPY public.tenants (id, prometheus, jaeger) FROM stdin;
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.users (id, name, password, token) FROM stdin;
+COPY public.users (id, username, password, token) FROM stdin;
 8e0016dd-5bec-470c-a26f-5e13b988801a	test2	$2a$10$mAgmPdb5I5T7g12MXs3q3OMKlymnhmh4NTrH8jH8vIyE.9CoBJP5C	5fb17a96-3e38-4211-963a-03d43583fe6d
 b8501716-4d8a-4bf8-a2e9-25a147bfe105	test1	$2a$10$cYL1eYqDVTjlF33ZHryl8OZxAo5iQwBGwRJ7OB7d6dJ2tjWbrbT9K	acfa769b-f2a9-4fb8-8fd3-9d8337159d46
 \.
